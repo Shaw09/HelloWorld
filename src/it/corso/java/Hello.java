ba46680f429805a8aa1a3cf6d9c578f56d48ff11
Shaw09 <michele_aliffi@yahoo.it>
2023-10-13 16:29:18 +0200

    First commit

---
+++ src/it/corso/java/Hello.java
@@ -0,0 +1,22 @@
+package it.corso.java;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		
+		int a = 10;
+
+		System.out.println(a);
+	}
+	
+	private String printXml() {
+		System.out.println("...");
+		
+		String test = "";
+		
+		
+		return "test";
+	}
+	
+}
---
+++ src/it/corso/util/Utils.java
@@ -0,0 +1,7 @@
+package it.corso.util;
+
+public class Utils {
+	public int a = 5;
+	
+	public double b = 0;
+}
